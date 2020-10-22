018cc25726a1246257b8c50fc1ddb7277a57a1ee
林文雄 <EX_110904150@hdjt.hdad.local>
2020-10-22 14:07:19 +0800

    提交

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = {
+    mode: 'development',
+    entry: "./src/index.js",
+        output: {
+        path: path.resolve(__dirname, 'dist'),
+            filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test:/\.css$/,use:['style-loader','css-loader']}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
+}
+module.exports = config
